0b2f6de98460986ad47599320478127ad7751fc6
GoRuGoo <dachanyr8989@gmail.com>
2023-06-11 19:50:42 +0900

    delete: Gormを使ったので不要なgolang-migrate用のマイグレーションファイルを削除

--- migrations/000001_create_reception.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reception(
-  uid VARCHAR(255) PRIMARY KEY NOT NULL,
-  mail VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  attends_first_day BOOLEAN NOT NULL,
-  attends_second_day BOOLEAN NOT NULL,
-  temperature_first_day FLOAT,
-  temperature_second_day FLOAT
- );
